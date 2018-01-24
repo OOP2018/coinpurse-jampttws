d66b9efe90e63466791541c1193e5d7c80332917
jampttws <tanasorn42@hotmail.com>
2018-01-24 23:31:46 +0700

    updated MoneyUtill

--- src/coinpurse/MoneyUtil.java
+++ src/coinpurse/MoneyUtil.java
@@ -13,8 +13,10 @@ public class MoneyUtil {
 		List<Coin> coins = new ArrayList<Coin>();
 		coins.add(new Coin(10,"Baht"));
 		coins.add(new Coin(5, "Rupie"));
+		coins.add(new Coin(35, "Baht"));
 		coins.add(new Coin(20, "Dollas"));
 		coins.add(new Coin(2, "Baht"));
+	    filterByCurrency(coins, "Baht");
 		printCoin(coins);
 		sortCoins(coins);
 		printCoin(coins);
@@ -24,10 +26,22 @@ public class MoneyUtil {
 	 * This method is use to print every coin in the list.
 	 * @param coins is the List of Coin object.
 	 */
-	public static void printCoin(List<Coin> coins){
+	static void printCoin(List<Coin> coins){
 		for(int i = 0; i < coins.size(); i++){
 			System.out.println(coins.get(i));
 		}
+	}
+	
+    static List<Coin> filterByCurrency(List<Coin> coins, String currency){
+    	List<Coin> filter = new ArrayList<Coin>();
+    	String checkCurrency;
+    	for(Coin c : coins){
+    		if(!c.getCurrency().equals(currency)){
+    			filter.add(c);
+    		}
+    	}
+    	for(Coin c : filter) coins.remove(c);
+	return coins;		
 	}
 	
 	/**
